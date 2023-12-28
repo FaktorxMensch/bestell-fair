944385f41fc1b351986b776a56764f490c271763
najajan-de <jan.natkow@gmail.com>
2023-12-29 00:51:02 +0100

    add restaurant list

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,15 @@
-<template>hw</template>
+<template>
+  <div>
+    <ul>
+      <li v-for="restaurant in restaurants">
+        {{ restaurant.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup>
+const supabase = useSupabaseClient()
+let { data: restaurants, error } = await supabase
+    .from('restaurants')
+    .select('*')
+</script>
