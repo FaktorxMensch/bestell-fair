32eefdb3f0f5b3d2cbe8fe140cce1ee9e94220d7
najajan-de <jan.natkow@gmail.com>
2023-12-29 21:25:46 +0100

    add add element

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,36 @@
         {{ restaurant.name }}
       </li>
     </ul>
+    <v-text-field
+        v-model="newRestaurantName"
+        label="New Restaurant Name"/>
+    <v-btn @click="addElement()">Add Element</v-btn>
   </div>
 </template>
 <script setup>
 const supabase = useSupabaseClient()
-let { data: restaurants, error } = await supabase
-    .from('restaurants')
-    .select('*')
+const restaurants = ref([])
+const fetchData = async function() {
+  const {data, error} = await supabase
+      .from('restaurants')
+      .select('*')
+  if (error) console.log(error)
+  else restaurants.value = data
+}
+await fetchData()
+const newRestaurantName = ref('')
+const addElement = async () => {
+  const { data, error } = await supabase
+      .from('restaurants')
+      .insert([
+        { name: newRestaurantName.value }
+      ])
+      .select('*')
+  if (error) console.log(error)
+  else {
+    restaurants.value = [...restaurants.value, ...data]
+    newRestaurantName.value = ''
+  }
+
+}
 </script>
