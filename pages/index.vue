6f1e48b00932bf9ae1324cf5ff0436b198423e53
najajan-de <jan.natkow@gmail.com>
2023-12-31 14:40:27 +0100

    add restaurant form

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,59 @@
         v-model="newRestaurantName"
         label="New Restaurant Name"/>
     <v-btn @click="addElement()">Add Element</v-btn>
+    <v-form @submit="addElement()">
+      <v-container>
+        <v-text-field variant="outlined" v-model="formData.name" label="Name"></v-text-field>
+        <v-text-field variant="outlined" v-model="formData.location" label="Location"></v-text-field>
+        <v-textarea variant="outlined" v-model="formData.description" label="Description"></v-textarea>
+        <v-text-field variant="outlined" v-model="formData.contact_email" label="Contact Email"></v-text-field>
+        <v-text-field variant="outlined" v-model="formData.contact_phone" label="Contact Phone"></v-text-field>
+        <v-select
+            variant="outlined"
+            v-model="formData.payment_methods"
+            :items="paymentMethods"
+            chips
+            label="Payment Methods"
+            multiple
+        ></v-select>
+        <!-- Hier sollte ein Feld für die 'order_fields' hinzugefügt werden, je nach Anforderungen -->
+        <!-- Hier sollte ein Feld für die 'products' hinzugefügt werden, je nach Anforderungen -->
+        <!-- Hier sollte ein Feld für die 'tags' hinzugefügt werden, je nach Anforderungen -->
+        <v-text-field variant="outlined" v-model="formData.website" label="Website"></v-text-field>
+        <v-btn type="submit" color="primary">Submit</v-btn>
+      </v-container>
+    </v-form>
+
   </div>
 </template>
 <script setup>
 const supabase = useSupabaseClient()
 const restaurants = ref([])
+const paymentMethods = [
+  'cash',
+  'card',
+  'paypal',
+  'applepay',
+  'googlepay',
+  'stripe',
+  'venmo',
+  'bitcoin',
+  'litecoin',
+  'ethereum',
+  'dogecoin',
+  'monero',
+  'other'
+]
+const formData = ref({
+  name: '',
+  location: '',
+  description: '',
+  contact_email: '',
+  contact_phone: '',
+  payment_methods: [],
+  tags: [],
+  website: '',
+})
 const fetchData = async function() {
   const {data, error} = await supabase
       .from('restaurants')
@@ -27,7 +75,7 @@ const addElement = async () => {
   const { data, error } = await supabase
       .from('restaurants')
       .insert([
-        { name: newRestaurantName.value }
+      formData.value
       ])
       .select('*')
   if (error) console.log(error)
