468b69f4454c62ce48e685d17488514e71bc7687
Dominik <dominik.ziegenhagel@gmail.com>
2023-12-31 15:39:40 +0100

    startseite mobile response

--- pages/login.vue
+++ pages/login.vue
@@ -14,18 +14,18 @@ const signInWithPassword = async () => {
 <template>
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title>Fairdish</v-toolbar-title>
+      <v-toolbar-title>fairDish</v-toolbar-title>
     </v-app-bar>
     <v-main class="flex">
       <v-row>
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="6" class="hidden md:flex">
           <img
               src="/partners/draw_authentication_fsn1.jpeg"
               class="h-full w-full bg-orange-400 object-cover"
           />
         </v-col>
-        <v-col cols="12" md="6" class="flex flex-col justify-center">
-          <v-form @submit.prevent="signInWithPassword" class="me-8 lg:px-8">
+        <v-col cols="12" md="6" class="flex flex-col justify-center min-h-48">
+          <v-form @submit.prevent="signInWithPassword" class="ms-8 me-16">
             <h1 class="text-4xl font-bold mb-8">Willkommen bei Fairdish Partner!</h1>
             <v-text-field autofocus="" variant="outlined" label="E-Mail" v-model="email" type="email"/>
             <v-text-field variant="outlined" label="Passwort" type="password" v-model="password"/>
