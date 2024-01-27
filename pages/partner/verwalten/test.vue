643fc7c33e7fc67ea62204649f3825a7d977f4dc
Dominik <dominik.ziegenhagel@gmail.com>
2024-01-27 14:01:20 +0100

    add test to gitignore

--- pages/partner/verwalten/test.vue
+++ pages/partner/verwalten/test.vue
@@ -1,60 +1,17 @@
 <template>
   <h1 class="text-3xl">Test</h1>
-  <div v-if="error">
-    {{ error }}
-  </div>
-  <div v-else>
-    <li v-for="order in orders" :key="order.id">
-      {{ order.id }}
-    </li>
-  </div>
+  <pre>
+    {{ data }}
+  </pre>
+
 </template>
 <script setup>
-const supabase = useSupabaseClient();
-let {data: orders, error} = await supabase
-    .from('orders')
+const supabase = useSupabaseClient()
+const orderId = 'aa9e8f40-0dc0-483f-8027-6a9811fcf575'; // Replace with the actual order ID
+
+const {data, error} = await supabase
+    .from('rlstest')
     .select('*')
-
-supabase
-    .channel('room1')
-    .on('postgres_changes', { event: '*', schema: 'public', table: 'orders' }, payload => {
-      console.log('Change received!', payload)
-    })
-    .subscribe()
-
-
-
-const channels = supabase.getChannels()
-console.log(channels)
-
-const channel = supabase.channel('room2')
-
-supabase
-    .channel('room2')
-    .on('broadcast', { event: 'cursor-pos' }, payload => {
-      console.log('Cursor position received!', payload)
-    })
-    .subscribe((status) => {
-      if (status === 'SUBSCRIBED') {
-        channel.send({
-          type: 'broadcast',
-          event: 'cursor-pos',
-          payload: { x: Math.random(), y: Math.random() },
-        })
-      }
-    })
-
-
-supabase
-    .channel('room2')
-    .subscribe((status) => {
-      if (status === 'SUBSCRIBED') {
-        channel.send({
-          type: 'broadcast',
-          event: 'cursor-pos',
-          payload: { x: Math.random(), y: Math.random()
-          },
-        })
-      }
-    })
+    // .eq('id', orderId)
+    // .limit(1);
 </script>
