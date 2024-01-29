35b76c70956875f3723ebd065e2d4c3cb92ed900
Dominik <dominik.ziegenhagel@gmail.com>
2024-01-29 01:14:53 +0100

    besser danke

--- pages/bestellung/danke.vue
+++ pages/bestellung/danke.vue
@@ -1,15 +1,12 @@
 <template>
-  <div class="h-full flex p-6 mt-16 flex-col items-center justify-center max-w-lg mx-auto text-center gap-2">
-    <v-icon size="80" icon="mdi-food"/>
-    <h1 class="text-4xl font-semibold p-4">Vielen Dank für deine Bestellung!</h1>
-    <p class="px-6 text-gray-500 text-center">Du erhältst in Kürze eine Bestellbestätigung per E-Mail,
+  <div class="h-full flex p-6 mt-24 flex-col items-center justify-center max-w-lg mx-auto text-center gap-2">
+<!--        <v-icon size="80" icon="mdi-food"/>-->
+    <img src="/landing/danke.jpeg" alt="Danke für deine Bestellung" class="w-72"/>
+    <h1 class="text-4xl font-semibold -mt-3 px-4">Vielen Dank für deine Bestellung!</h1>
+    <p class="px-6 text-gray-500 mt-3 text-center">Du erhältst in Kürze eine Bestellbestätigung per E-Mail,
       in der du deine Bestellung stornieren und deinen Bestellstatus verfolgen kannst.</p>
-    <v-btn href="/" class="mt-4" color="black"
-           prepend-icon="mdi-home"
-           rounded variant="flat" size="large">
-      Startseite
-    </v-btn>
-    <div class="h-32"></div>
+    <!--    <v-btn href="/" class="mt-4" color="black" prepend-icon="mdi-home" rounded variant="flat" size="large"> Startseite </v-btn>-->
+    <div class="h-24"></div>
   </div>
 </template>
 <script setup>
