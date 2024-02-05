e10957b89da00869addd110f62ae2dd9b7921538
Dominik <dominik.ziegenhagel@gmail.com>
2024-02-05 08:14:43 +0100

    every 50 mimutes refreshSession

--- pages/partner/inbox/index.vue
+++ pages/partner/inbox/index.vue
@@ -3,8 +3,7 @@
 const route = useRoute()
 // const restaurant = route.params.restaurant
 const inboxStore = useInboxStore()
-inboxStore.init() //Insert restaurant id here
-
+inboxStore.init()
 //Get and set theme to local storage
 
 const theme = ref(localStorage.getItem('theme') || 'dark')
@@ -12,7 +11,7 @@ watch(theme, (value) => {
   localStorage.setItem('theme', value)
 })
 
-function toggleTheme () {
+function toggleTheme() {
   console.log('toggleTheme')
   theme.value = theme.value === 'light' ? 'dark' : 'light'
 }
@@ -20,6 +19,31 @@ function toggleTheme () {
 // TODo: Need some param to check if orders are pending??
 // ToDo: WHat to show in the table if no orders exist?
 
+const user = useSupabaseUser()
+watch(user, (value) => {
+  if (!value) {
+    // nur wenn wir eine user id haben, sind wir eingeloogt und dürfen bleiben
+    navigateTo('/login')
+  }
+}, {immediate: true})
+
+// every 50 mimutes refreshSession()
+const supabase = useSupabaseClient()
+setInterval(async () => {
+  const {data, error} = await supabase.auth.refreshSession()
+  if (error) {
+    console.error('Error refreshing session', error)
+    await Swal.fire({
+      title: 'Du wurdest ausgeloggt',
+      text: error.message,
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    })
+    navigateTo('/login')
+    return
+  }
+  console.log('Session refreshed!', data)
+}, 50 * 60 * 1000)
 </script>
 <template>
   <v-theme-provider :theme="theme">
@@ -27,8 +51,8 @@ function toggleTheme () {
       <partner-inbox-actionbar :theme="theme" @toggleTheme="toggleTheme"/> <!-- :refresh="refresh"/>-->
       <v-main>
         <v-container fluid>
-<!--          <v-progress-linear v-if="data.pending" indeterminate/>-->
-          <partner-inbox-orders-table /> <!-- v-else/> -->
+          <!--          <v-progress-linear v-if="data.pending" indeterminate/>-->
+          <partner-inbox-orders-table/> <!-- v-else/> -->
         </v-container>
       </v-main>
     </v-app>
