548804faf737660838392abea526bf5aef8e43d6
Dominik <dominik.ziegenhagel@gmail.com>
2024-04-18 21:37:04 +0200

    failing test

--- cypress/e2e/smoketest.cy.js
+++ cypress/e2e/smoketest.cy.js
@@ -2,5 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
     cy.get('h1').should('contain', 'Hunger')
+    cy.get('h1').should('contain', 'Kein Hunger')
+
   })
 })
