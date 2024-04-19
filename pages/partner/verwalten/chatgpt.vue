3a1d5e110d26bd6dc9dc23b04a5887e8a763e087
Dominik <dominik.ziegenhagel@gmail.com>
2024-04-19 10:12:52 +0200

    chatgpt prompt beilegen

--- pages/partner/verwalten/chatgpt.vue
+++ pages/partner/verwalten/chatgpt.vue
@@ -1,6 +1,6 @@
 <template>
   <partner-verwalten-nav-header
-      subtitle="Bearbeite Dieses JSON mit ChatGPT und klicke auf Änderungen laden.">
+      subtitle="Bearbeite Dieses JSON mit ChatGPT und klicke dann auf Änderungen laden.">
 
   </partner-verwalten-nav-header>
   <v-card>
@@ -8,7 +8,12 @@
       Mit ChatGPT bearbeiten
     </v-card-title>
     <v-card-text>
-      <v-textarea v-model="restaurantAsJSON" outlined dense rows="10"/>
+      <v-textarea v-model="prompt" outlined dense
+                  prepend-inner-icon="mdi-information"
+                  rows="10"/>
+      <v-textarea v-model="restaurantAsJSON" outlined dense rows="10"
+                  prepend-inner-icon="mdi-message-text"
+                  placeholder="JSON hier einfügen"/>
       <v-btn text="Änderungen laden"
              variant="flat"
              prepend-icon="mdi-import"
@@ -22,6 +27,17 @@
 definePageMeta({layout: 'partner-verwalten'})
 const verwaltenStore = useVerwaltenStore()
 const restaurantAsJSON = ref(JSON.stringify(verwaltenStore.restaurant))
+const prompt = ref(`Du hilfst beim Einpflegen von Restaurants in einen Bestellservice. Das Ausgabeformat ist ein riesen JSON String pro Restaurant.
+Der User wird Dir vielleicht auch eine Menge Informationen irgendwo zufällig herkopieren. Versuche das auch in diesen Kontext zu parsen.
+Du holst Dir vom User alle Infos ein, sofern er sie Dir nicht bereits gegeben hat.
+Antworte auf keinen Fall, bevor Du nicht alle Informationen hast.
+Insbesondere zu den Öffnungszeiten (0=Mo,1=Di,...) und der Speisekarte sowie allen Konfigurationsoptionen innerhalb der Speisekarte.
+In diesem Fall gibt er Dir bereits ein JSON Schema von einem existierenden Restaurant, das Du anpassen sollst, ohne die Struktur zu verändern.
+
+Wenn Du dir unsicher bist, frage den User nach mehr Informationen und weigere Dich zu antworten, bevor Du nicht alle Informationen hast.
+Wenn Du zum Beispiel nicht weisst, wie das JSON Format an einer Stelle auszusehen hat, frage den User dass er dass bitte im GUI beispielhaft einträgt und dann erneut die Anfrage stellt.
+
+Danach antwortest Du in folgendem validen JSON Schema:`)
 const importRestaurant = async () => {
   try {
     const restaurant = JSON.parse(restaurantAsJSON.value)
