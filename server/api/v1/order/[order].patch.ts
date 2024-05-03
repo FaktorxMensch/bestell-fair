16474d2381cb36a44830358cbf6a5985f5d15f9b
Dominik <dominik.ziegenhagel@gmail.com>
2024-05-03 23:53:25 +0200

    methoden vereinen

--- server/api/v1/order/[order].patch.ts
+++ server/api/v1/order/[order].patch.ts
@@ -1,91 +1,28 @@
 import {createTransport} from 'nodemailer';
 import {supabaseServiceRole as supabase} from "~/composables/supabaseServiceRole";
 import {config} from 'dotenv'
-
+import {getOrder, sendMail} from "~/server/api/v1/order/[order]/notify.post";
 config()
 
 export default defineEventHandler(async event => {
 
+    const {order, error} = await getOrder(event.context.params.order)
+    if (error) return {error}
 
-        const {
-            data: order,
-            error: orderError
-        } = await supabase.from('orders').select().eq('id', event.context.params.order)
-            .single()
+    const dirrentPickup = order.pickup_at_init !== order.pickup_at ? ` - Geänderte Abholzeit!` : '';
+    const ifDifferentPickup = order.pickup_at_init !== order.pickup_at ? `<p>Bitte beachte: Die geplante Abholung wurde geändert auf: <b>${new Date(order.pickup_at).toLocaleTimeString('de-DE')} Uhr</b> (ursprünglich  ${new Date(order.pickup_at_init).toLocaleTimeString('de-DE')})</p>` : `<p>Aktuelle Abholung: ${new Date(order.pickup_at).toLocaleTimeString('de-DE')}</p>`;
 
-        if (!order) {
-            return {
-                error: orderError
-            }
-        }
+    const mailOptions = {
+        subject: '[' + order.status + dirrentPickup + '] ' + 'Deine Bestellung über Bestell Fair!', // Subject line
+        html: `
+        <h1>Bestellung</h1>
+        <p>Deine Bestellung hat den Status <strong>${order.status}</strong></p>
+        ${ifDifferentPickup}
+        <p><a href="https://bestell-fair.de/bestellung/${order.id}">Weitere Informationen zur Bestellung ansehen &rarr;</a></p>
+        <p>Vielen Dank für deine Bestellung!</p>
+        <p>Dein Bestell Fair! Team</p>`
+    };
+    await sendMail(mailOptions)
 
-        if (!order.receive_email_updates) {
-            return {
-                success: true,
-                message: 'User does not want to receive email updates'
-            }
-        }
-
-        console.log('order', order)
-
-        // Create reusable transporter object using SMTP transport
-        const transporter = createTransport({
-            host: process.env.MAIL_SMTP_HOST,
-            port: process.env.MAIL_SMTP_PORT,
-            secure: false, // secure:true for port 465, secure:false for port 587
-            auth: {
-                user: process.env.MAIL_SMTP_USER,
-                pass: process.env.MAIL_SMTP_PASS
-            },
-        });
-
-
-        console.log('transporter', transporter)
-
-    console.log('init pick up', order.pickup_at_init)
-    console.log('current pick up', order.pickup_at)
-
-    // didi it change
-    console.log('did it change', order.pickup_at_init !== order.pickup_at)
-
-        const ifDifferentPickup = order.pickup_at_init !== order.pickup_at ? `<p>Bitte beachte: Die geplante Abholung wurde geändert auf: <b>${new Date(order.pickup_at).toLocaleString('de-DE')} Uhr</b> (ursprünglich  ${new Date(order.pickup_at_init).toLocaleString('de-DE')})</p>` : `<p>Aktuelle Abholung: ${new Date(order.pickup_at).toLocaleString('de-DE')}</p>`;
-        const dirrentPickup = order.pickup_at_init !== order.pickup_at ? `Geänderte Abholzeit - ` : ' ';
-
-        // Setup email data
-        const mailOptions = {
-            from: '"Bestell Fair!" <noreply@bestell-fair.de>',
-            to: 'info@faktorxmensch.com',                  // list of receivers
-            subject: '[' + order.status + '] ' + dirrentPickup + 'Deine Bestellung über Bestell Fair!', // Subject line
-            html: `
-    <h1>Bestellung</h1>
-    <p>Deine Bestellung hat den Status <strong>${order.status}</strong></p>
-    ${ifDifferentPickup}
-    <p><a href="https://bestell-fair.de/bestellung/${order.id}">Weitere Informationen zur Bestellung ansehen &rarr;</a></p>
-    <p>Vielen Dank für deine Bestellung!</p>
-    <p>Dein Bestell Fair! Team</p>
-            `
-        };
-
-        // Send mail with defined transport object
-        try {
-            const info = await transporter.sendMail(mailOptions);
-            console.log('Message sent: %s', info.messageId);
-
-            // Email sent successfully
-            return {
-                success: true,
-                message: 'Email sent successfully'
-            };
-        } catch (error) {
-            console.error('Error sending email: ', error);
-
-            // Failed to send email
-            return {
-                success: false,
-                message: 'Failed to send email'
-            };
-
-        }
-    }
-)
+})
 
--- server/api/v1/order/[order].post.ts
+++ server/api/v1/order/[order].post.ts
@@ -1,18 +1,18 @@
 import {createTransport} from 'nodemailer';
 import {supabaseServiceRole as supabase} from "~/composables/supabaseServiceRole";
 import {config} from 'dotenv'
-
+import {getOrder, sendMail} from "~/server/api/v1/order/[order]/notify.post";
 config()
 
 export default defineEventHandler(async event => {
 
+    const {order, error} = await getOrder(event.context.params.order)
+    if (error) return {error}
 
-        const {
-            data: order,
-            error: orderError
-        } = await supabase.from('orders').select().eq('id', event.context.params.order)
-            .single()
+    const dirrentPickup = order.pickup_at_init !== order.pickup_at ? ` - Geänderte Abholzeit!` : '';
+    const ifDifferentPickup = order.pickup_at_init !== order.pickup_at ? `<p>Bitte beachte: Die geplante Abholung wurde geändert auf: <b>${new Date(order.pickup_at).toLocaleTimeString('de-DE')} Uhr</b> (ursprünglich  ${new Date(order.pickup_at_init).toLocaleTimeString('de-DE')})</p>` : `<p>Aktuelle Abholung: ${new Date(order.pickup_at).toLocaleTimeString('de-DE')}</p>`;
 
+<<<<<<< HEAD
         if (!order) {
             return {
                 error: orderError
@@ -74,4 +74,18 @@ export default defineEventHandler(async event => {
         }
     }
 )
+=======
+    const mailOptions = {
+        subject: '[' + order.status + dirrentPickup + '] ' + 'Deine Bestellung über Bestell Fair!', // Subject line
+        html: `
+        <h1>Vielen Dank</h1>
+        <p>Deine Bestellung ist eingegangen, wir informieren dich per E-Mail, <b>sobald das Restaurant deine Bestellung bestätigt hat</b>.</p>
+        <p>Geplante Abholung: ${new Date(order.pickup_at).toLocaleTimeString('de-DE')}</p>
+        <p><a href="https://bestell-fair.de/bestellung/${order.id}">Weitere Informationen zur Bestellung ansehen &rarr;</a></p>
+        <p>Vielen Dank für deine Bestellung!</p>
+        <p>Dein Bestell Fair! Team</p>`
+    };
+    await sendMail(mailOptions)
+>>>>>>> b4759af (methoden vereinen)
 
+})
